d0903de747af684f8cb380939eb386c1b3abc36d
Administrator <1967530737@qq.com>
2020-05-02 09:37:32 +0800

    重新创建

---
+++ CourseDesign/CourseDesignDlg.h
@@ -0,0 +1,36 @@
+﻿
+// CourseDesignDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCourseDesignDlg 对话框
+class CCourseDesignDlg : public CDialogEx
+{
+// 构造
+public:
+	CCourseDesignDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_COURSEDESIGN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
